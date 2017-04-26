f90ad872e7dcaf0df2877cd57ae144d8f04033d6
th720309 <646595546@qq.com>
2017-04-26 15:34:02 +0800

    change index

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Nicholas on 2017/4/25.
+ */
+
+var weixin = document.querySelector(".icon-weixin");
+var codeimg = document.querySelector(".codeimg");
+weixin.addEventListener('click',function () {
+    codeimg.style.display="block";
+})
